642851ea11c0e93a293039c55d294d2030dbfca3
井上頌英 <inoueshouei@fuck-mini.local>
2025-08-14 22:18:11 +0900

    og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,35 @@ import "./globals.css";
 
 const outfit = Outfit({
 	subsets: ["latin"],
-	weight: ["300", "400", "700"], // 300を追加してより細いウェイトを利用可能に
-	variable: "--font-outfit", // CSS変数名
+	weight: ["300", "400", "700"],
+	variable: "--font-outfit",
 	display: "swap",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Inoue Nobuhide",
-	description: "My Portfolio",
+	description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
+	openGraph: {
+		title: "Inoue Nobuhide",
+		description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
+		url: "https://inouenobuhide.dev",
+		siteName: "Inoue Nobuhide",
+		images: [
+			{
+				url: "https://inouenobuhide.dev/og-image.png", // 公開されている絶対URL
+				width: 1200,
+				height: 630,
+			},
+		],
+		locale: "ja_JP",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Inoue Nobuhide",
+		description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
+		images: ["https://inouenobuhide.dev/og-image.png"],
+	},
 };
 
 export default function RootLayout({
