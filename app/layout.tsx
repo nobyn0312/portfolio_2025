9b2183ed03cfa85b00c03cc31838efea7b0a07f4
井上頌英 <inoueshouei@fuck-mini.local>
2025-08-14 23:42:42 +0900

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,46 +3,46 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-	subsets: ["latin"],
-	weight: ["300", "400", "700"],
-	variable: "--font-outfit",
-	display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-outfit",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-	title: "Inoue Nobuhide",
-	description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
-	openGraph: {
-		title: "Inoue Nobuhide",
-		description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
-		url: "https://inouenobuhide.dev",
-		siteName: "Inoue Nobuhide",
-		images: [
-			{
-				url: "https://inouenobuhide.dev/images/og-image.png", // 公開されている絶対URL
-				width: 1200,
-				height: 630,
-			},
-		],
-		locale: "ja_JP",
-		type: "website",
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "Inoue Nobuhide",
-		description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
-		images: ["https://inouenobuhide.dev/images/og-image.png"],
-	},
+  title: "Inoue Nobuhide",
+  description: "My Portfolio",
+  openGraph: {
+    title: "Inoue Nobuhide Portfolio",
+    description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
+    url: "https://inouenobuhide.dev",
+    siteName: "Inoue Nobuhide",
+    type: "website",
+    locale: "ja_JP",
+    images: [
+      {
+        url: "https://inouenobuhide.dev/images/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Inoue Nobuhide Portfolio",
+    description: "Web制作・3DCG・音楽などを手がけるポートフォリオサイト",
+    images: ["https://inouenobuhide.dev/images/og-image.png"],
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body className={`${outfit.variable} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="ja">
+      <body className={`${outfit.variable} antialiased`}>{children}</body>
+    </html>
+  );
 }
