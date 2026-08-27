01d10131058f0b11ad227a0045643db54f47da93
nobyn0312 <nobyn0312@gmail.com>
2026-08-27 16:02:37 +0900

    next.jsバージョンアップ

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	// Home 直下の package-lock.json を誤検出しないようにする
+	outputFileTracingRoot: path.join(__dirname),
+	turbopack: {
+		root: path.join(__dirname),
+	},
 };
 
 export default nextConfig;
